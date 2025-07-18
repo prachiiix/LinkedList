bcc7c5c3090c443bc8760a4480ee8e2a8dbb1f28
Prachi Chougule <prachichougule.009@gmail.com>
2025-07-18 04:24:54 -0700

    Update hardCode.java

--- hardCode.java
+++ hardCode.java
@@ -1,28 +1,27 @@
 import java.util.*;
 
-class Node{
+class Node {
     int info;
     Node link;
 }
 
-class Linkedlist{
+class Linkedlist {
     static Scanner sc = new Scanner(System.in);
     static Node start = null;
 
-    public static Node createNode(){
+    public static Node createNode() {
         return new Node();
     }
 
-    public static void insertnode(){
+    public static void insertnode() {
         Node temp = createNode();
         System.out.println("Enter an element: ");
         temp.info = sc.nextInt();
         temp.link = null;
 
-        if(start == null){
+        if (start == null) {
             start = temp;
-        }
-        else{
+        } else {
             Node t1 = start;
             while (t1.link != null) {
                 t1 = t1.link;
@@ -31,24 +30,49 @@ class Linkedlist{
         }
     }
 
-    public static void deleteNode(){
-        if(start == null){
+    public static void deleteNode() {
+        if (start == null) {
             System.out.println("List is empty");
-        }
-        else{
+        } else {
             start = start.link;
         }
     }
 
-    public static void printList(){
-        if(start == null){
+    public static void printList() {
+        if (start == null) {
             System.out.println("List is empty");
-        }
-        else{
+        } else {
             Node t2 = start;
-            while(t2 != null){
+            while (t2 != null) {
                 System.out.print(t2.info + "->");
                 t2 = t2.link;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static int menu() {
+        System.out.println("menu: \n1. insert\n2. delete\n3. print\n4. exit");
+        System.out.println("Enter choice: ");
+        return sc.nextInt();
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            switch (menu()) {
+                case 1:
+                    insertnode();
+                    break;
+                case 2:
+                    deleteNode();
+                    break;
+                case 3:
+                    printList();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice");
             }
         }
     }
